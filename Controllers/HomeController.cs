8cd82826ac7d9448b7756af205a43c3510121e16
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-07-03 16:33:28 +0700

    Áp dụng công nghệ phân trang

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,21 +2,34 @@
 using Microsoft.AspNetCore.Mvc;
 using QLHocSinh_LT.Models;
 using QLHocSinh_LT.Models.StudentF;
+using QLHocSinh_LT.Models.ViewModels;
 
 namespace QLHocSinh_LT.Controllers;
 
 public class HomeController : Controller
 {
     private IStudentRepository repo;
+    public int PageSize = 2;
 
     public HomeController(IStudentRepository repo)
     {
         this.repo = repo;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(int studentPage = 1)
     {
-        return View(repo.Students);
+        return View( new StudentsListViewModel
+        {
+            Students = repo.Students
+            .OrderBy(s => s.Id)
+            .Skip((studentPage - 1) * PageSize)
+            .Take(PageSize),
+            PagingInfo = new PagingInfo 
+            {
+                CurrentPage = studentPage,
+                ItemsPerPage = PageSize,
+                TotalItems = repo.Students.Count()
+            }
+        });
     }
-
 }
---
+++ Models/ViewModels/StudentsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using QLHocSinh_LT.Models.StudentF;
+
+namespace QLHocSinh_LT.Models.ViewModels
+{
+    public class StudentsListViewModel
+    {
+        public IEnumerable<Student> Students { get; set; } = Enumerable.Empty<Student>();
+        public PagingInfo PagingInfo { get; set; } = new();
+    }
+}

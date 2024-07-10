64d757517a86c360ce349e55e7587184b439423d
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-07-10 16:41:29 +0700

    Thêm tính năng hiển thị danh sách Khoa

---
+++ Controllers/FacultiesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QLHocSinh_LT.Models.FacultyF;
+using QLHocSinh_LT.Models.StudentF;
+using QLHocSinh_LT.Models.ViewModels;
+
+namespace QLHocSinh_LT.Controllers
+{
+    public class FacultiesController : Controller
+    {
+        private IFacultyRepository repo;
+        public int PageSize = 2;
+
+        public FacultiesController(IFacultyRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        // GET: Faculties
+        public IActionResult Index(int currentPage = 1)
+            => View(new FacultiesListViewModel
+            {
+                Faculties = repo.Faculties
+                    .OrderBy(s => s.Id)
+                    .Skip((currentPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = currentPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repo.Faculties.Count()
+                }
+            });
+    }
+}
---
+++ Models/ViewModels/FacultiesListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using QLHocSinh_LT.Models.FacultyF;
+
+namespace QLHocSinh_LT.Models.ViewModels
+{
+    public class FacultiesListViewModel
+    {
+        public IEnumerable<Faculty> Faculties { get; set; } = Enumerable.Empty<Faculty>();
+        public PagingInfo PagingInfo { get; set; } = new();
+    }
+}

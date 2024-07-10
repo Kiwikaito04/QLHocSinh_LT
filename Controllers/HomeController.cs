c95ea3c09542bfe0c8005c6bb566815f0121f14c
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-07-10 12:49:51 +0700

    Chuyển trang index của Student sang controlller students

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,28 +8,5 @@ namespace QLHocSinh_LT.Controllers;
 
 public class HomeController : Controller
 {
-    private IStudentRepository repo;
-    public int PageSize = 2;
-
-    public HomeController(IStudentRepository repo)
-    {
-        this.repo = repo;
-    }
-
-    public IActionResult Index(int studentPage = 1)
-    {
-        return View( new StudentsListViewModel
-        {
-            Students = repo.Students
-            .OrderBy(s => s.Id)
-            .Skip((studentPage - 1) * PageSize)
-            .Take(PageSize),
-            PagingInfo = new PagingInfo 
-            {
-                CurrentPage = studentPage,
-                ItemsPerPage = PageSize,
-                TotalItems = repo.Students.Count()
-            }
-        });
-    }
+    
 }
---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QLHocSinh_LT.Models.StudentF;
+using QLHocSinh_LT.Models.ViewModels;
+
+namespace QLHocSinh_LT.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private IStudentRepository repo;
+        public int PageSize = 2;
+
+        public StudentsController(IStudentRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        // GET: Students
+        public IActionResult Index(int studentPage = 1)
+        {
+            return View(new StudentsListViewModel
+            {
+                Students = repo.Students
+                .OrderBy(s => s.Id)
+                .Skip((studentPage - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = studentPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repo.Students.Count()
+                }
+            });
+        }
+
+
+
+    }
+}

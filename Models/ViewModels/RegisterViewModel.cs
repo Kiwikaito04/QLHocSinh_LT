da88b80aa19470abba25e13f5cc5e5d4277326fa
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-07-16 09:50:37 +0700

    Chỉnh sửa lại View đăng ký

--- Models/ViewModels/RegisterViewModel.cs
+++ Models/ViewModels/RegisterViewModel.cs
@@ -1,20 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace QLHocSinh_LT.Models.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
+        [DisplayName("Tên tài khoản")]
+        [Required(ErrorMessage = "Tên tài khoản là bắt buộc")]
         public string Username { get; set; } = string.Empty;
 
-        [Required]
+        [DisplayName("Email tài khoản")]
+        [Required(ErrorMessage = "Email là bắt buộc")]
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
 
-        [Required]
+        [DisplayName("Mật khẩu")]
+        [Required(ErrorMessage = "Mật khẩu là bắt buộc")]
         [DataType(DataType.Password)]
         public string Password { get; set; } = string.Empty;
 
+        [DisplayName("Xác nhận mật khẩu")]
+        [Required(ErrorMessage = "Cần nhập lại xác nhận mật khẩu")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Mật khẩu không trùng nhau")]
         public string ConfirmPassword { get; set; } = string.Empty;

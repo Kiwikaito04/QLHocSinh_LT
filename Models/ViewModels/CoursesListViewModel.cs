d4fa94e4ce93a8230004767c36dc868f1b44b98c
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-07-17 15:17:58 +0700

    Thêm danh sách Môn học

---
+++ Models/ViewModels/CoursesListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using QLHocSinh_LT.Models.CourseF;
+using System.ComponentModel.DataAnnotations;
+
+namespace QLHocSinh_LT.Models.ViewModels
+{
+    public class CoursesListViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; } = Enumerable.Empty<Course>();
+        public PagingInfo PagingInfo { get; set; } = new();
+    }
+}
